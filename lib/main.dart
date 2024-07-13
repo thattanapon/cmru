a143b64aaed8ff246443d7b1caf58f9eb9dd498c
thattanapon bunsombat <64143133@g.cmru.ac.th>
2024-07-13 16:23:12 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:cmru/screen/home_screen.dart';
+import 'package:cmru/screen/login_screen.dart';
+import 'package:cmru/screen/register_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(),
+        '/home': (context) => HomeScreen(),
+      },
+    );
+  }
+}
